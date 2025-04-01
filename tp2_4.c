fb8aa78fe261f5e29fca03206f43ae37f67ee7e5
Nicolas Powell <nicopowell30@gmail.com>
2025-04-01 12:07:54 -0300

    se agrego la estructura compu con unos comentarios descriptivos

--- tp2_4.c
+++ tp2_4.c
@@ -4,7 +4,7 @@ struct compu {
   int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3)
   int anio; // Año de fabricación (valor entre 2015 y 2024)
   int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8)
-  char *tipo_cpu;
+  char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres)
 };
 
 
